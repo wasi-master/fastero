f0ec7f4f34ba0b9c6d9c68e893bc4d1cfb4cd43c
Wasi Master <arianmollik323@gmail.com>
2022-04-13 12:25:00 +0600

    🔖 Bump version to 0.1.0

--- fastero/__init__.py
+++ fastero/__init__.py
@@ -3,6 +3,6 @@
 __name__ = "fastero"
 __title__ = __name__
 __license__ = "MIT"
-__version__ = "0.1.0rc1"
+__version__ = "0.1.0"
 __author__ = "Arian Mollik Wasi"
 __github__ = "https://github.com/wasi-master/fastero"
